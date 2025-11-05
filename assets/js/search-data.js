30a2236c4f9d34507bbd7646e32c69e5cbf1c4d2
baolade <meixian.li@connect.polyu.hk>
2025-11-05 20:10:16 +0000

    Deploying to gh-pages from @ baolade/baolade.github.io@32bac9e66bd2dd0c9bffa33e0d0eaa962848b35f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-fun-stuff",
           title: "Fun stuff",
           description: "Vicky Off Duty",
